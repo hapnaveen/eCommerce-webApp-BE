8553dc706a6cc47cf74d17ab33a60bc4eee877a2
hapnaveen <naveen.hedallaarachchi@gmail.com>
2024-06-01 19:27:50 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB database!'))
+.catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
